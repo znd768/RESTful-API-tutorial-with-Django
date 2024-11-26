d32588c62ccba1a98b0699fcf519cadad1312639
znd768 <rasksk37@gmail.com>
2024-11-27 00:23:46 +0900

    created project named rental

--- apiTutorial/settings.py
+++ apiTutorial/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rental.apps.RentalConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
