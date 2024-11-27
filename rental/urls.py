86281c8f641173ec6190f40cb7515bdd2f3e86bc
znd768 <rasksk37@gmail.com>
2024-11-27 22:55:44 +0900

    changed the view to use generics

--- rental/urls.py
+++ rental/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from rental import views
 
 urlpatterns = [
-    path('offers/', views.offer_list),
-    path('offers/<int:pk>/', views.offer_detail),
+    path('offers/', views.OfferList.as_view()),
+    path('offers/<int:pk>/', views.OfferDetails.as_view()),
 ]
--- rental/views.py
+++ rental/views.py
@@ -1,40 +1,12 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import generics
 from rental.models import Offer
 from rental.serializers import OfferSerializer
 
 
-@api_view(['GET', 'POST'])
-def offer_list(request):
-    if request.method == 'GET':
-        offers = Offer.objects.all()
-        serializer = OfferSerializer(offers, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = OfferSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class OfferList(generics.ListCreateAPIView):
+    queryset = Offer.objects.all()
+    serializer_class = OfferSerializer
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def offer_detail(request, pk):
-    try:
-        offer = Offer.objects.get(pk=pk)
-    except Offer.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = OfferSerializer(offer)
-        return Response(serializer.data)
-    elif request.method == 'PUT':
-        serializer = OfferSerializer(offer, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        offer.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class OfferDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Offer.objects.all()
+    serializer_class = OfferSerializer
