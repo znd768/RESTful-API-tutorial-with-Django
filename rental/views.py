cf63016f3f32676320d9f789d38f267365024c8c
znd768 <rasksk37@gmail.com>
2024-11-27 22:16:22 +0900

    created first view

--- rental/views.py
+++ rental/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rental.models import Offer
+from rental.serializers import OfferSerializer
 
-# Create your views here.
+
+@api_view(['GET', 'POST'])
+def offer_list(request):
+    if request.method == 'GET':
+        offers = Offer.objects.all()
+        serializer = OfferSerializer(offers, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = OfferSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
