8ef95985098ab6766374e7cb5dfb6e6c8a68561d
znd768 <rasksk37@gmail.com>
2024-11-28 22:22:45 +0900

    added permission to POST requests

--- rental/serializers.py
+++ rental/serializers.py
@@ -1,8 +1,17 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from rental.models import Offer
 
 
 class OfferSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
     class Meta:
         model = Offer
-        fields = ['id', 'address', 'size', 'type', 'price', 'sharing', 'text']
+        fields = ['id', 'address', 'size', 'type', 'price', 'sharing', 'text', 'author']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    offers = serializers.PrimaryKeyRelatedField(many=True, queryset=Offer.objects.all())
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'offers']
--- rental/views.py
+++ rental/views.py
@@ -1,12 +1,26 @@
 from rest_framework import generics
+from rest_framework import permissions
 from rental.models import Offer
-from rental.serializers import OfferSerializer
+from rental.serializers import OfferSerializer, UserSerializer
+from django.contrib.auth.models import User
 
 
 class OfferList(generics.ListCreateAPIView):
     queryset = Offer.objects.all()
     serializer_class = OfferSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
 class OfferDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Offer.objects.all()
     serializer_class = OfferSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetails(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
